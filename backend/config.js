2a94113dc5b9585518a9ddd83ebd4921e893d257
Javid Mushtan <javidmushtan@gmail.com>
2025-06-12 19:39:24 +0530

    config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;//backend port number
-export const mongoDBURL = "mongodb://localhost:27017/BOOK";
+export const mongoDBURL = "mongodb://localhost:27017/BOOK";// you can change BOOK to your database name
